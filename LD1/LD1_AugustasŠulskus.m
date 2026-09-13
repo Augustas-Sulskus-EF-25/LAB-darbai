d70a77c8916341b6185b160f1c7f35de0a657ebf
Augustas-Sulskus-EF-25 <a.sulskutis@gmail.com>
2026-09-13 17:10:39 +0300

    Implement tasks in LD1_AugustasŠulskus.m
    Add initial tasks for plotting and matrix operations.

---
+++ LD1/LD1_AugustasŠulskus.m
@@ -0,0 +1,30 @@
+%{
+pirma uzduotis
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]    |    F_2 [-x-]')
+
+help sin;
+help plot;
+help title;
+
+doc sin;
+doc plot;
+doc title;
+%}
+
+%{
+antra užduotis
+N = 4;
+v = N+1:0.5:N+4;
+A=[4 5 6; 5 6 7; 6 7 8];
+A(3,2)
+A(2:3, 1:2)
+B = A([1 3], [1 3])
+A = [A v(1:3)']
+%}
