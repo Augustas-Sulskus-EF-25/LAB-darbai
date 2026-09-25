f414b1f9f3c3145f0400c6aab1563c4c65a994f3
Augustas-Sulskus-EF-25 <a.sulskutis@gmail.com>
2026-09-25 11:53:28 +0300

    Add plots for y = x^3 + tan(x) and exponential functions

--- LD3/LD3_AugustasŠulskus.m
+++ LD3/LD3_AugustasŠulskus.m
@@ -1,1 +1,41 @@
+%1 Dvimatis grafikų vaizdavimas
+
+figure;
+
+x = 0:0.1:2*pi;
+y = x.^3 + tan(x);
+plot(x, y, 'b-');
+
+xlabel('x');
+ylabel('y');
+title('Funkcija y = x^3 + tan(x)');
+
+axis([min(x) max(x) min(y) max(y)]);
+
+legend('y = x^3 + tan(x)', 'Location', 'bestoutside');
+
+grid on;
+
+
+figure;
+
+x = 0:0.1:2*pi;
+y1 = exp(x);
+y2 = exp(3*x);
+y3 = exp(5*x);
+
+plot(x, y1, 'b-o', ...
+    x, y2, 'g-o', ...
+    x, y3, 'r-o');
+
+xlabel('x');
+ylabel('y');
+title('Eksponentinės funkcijos');
+
+axis([min(x) max(x) min([y1 y2 y3]) max([y1 y2 y3])]);
+
+legend('y = e^x', 'y = e^{3x}', 'y = e^{5x}', ...
+    'Location', 'bestoutside');
+
+grid on;
 
