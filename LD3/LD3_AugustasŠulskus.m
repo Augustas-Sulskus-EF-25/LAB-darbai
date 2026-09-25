30029610e9e9547e32f504330aef6b7de82b9c93
Augustas-Sulskus-EF-25 <a.sulskutis@gmail.com>
2026-09-25 18:45:00 +0300

    Implement specialized graph creation in LD3_AugustasŠulskus.m
    Added specialized graph creation with bar charts for student data visualization.

--- LD3/LD3_AugustasŠulskus.m
+++ LD3/LD3_AugustasŠulskus.m
@@ -1,4 +1,5 @@
-%1 Dvimatis grafikų vaizdavimas
+%{
+1 Dvimatis grafikų vaizdavimas
 
 figure;
 
@@ -38,4 +39,66 @@ legend('y = e^x', 'y = e^{3x}', 'y = e^{5x}', ...
     'Location', 'bestoutside');
 
 grid on;
+}%
 
+%{
+ 
+2 Specializuotų grafikų kūrimas
+
+A = [
+    8  7  9  10;
+    6  8  7   9;
+    10 9  8   9;
+    7  6  8   7;
+    9  10 9   8;
+    5  7  6   8
+];
+
+%Vertikalių stulpelių diagrama
+figure;
+subplot(2,1,1);
+b = bar(A);
+
+b(1).FaceColor = [0 0 0];       
+b(2).FaceColor = [0.3 0.3 0.3]; 
+b(3).FaceColor = [0.7 0.7 0.7]; 
+b(4).FaceColor = [1 1 1];       
+
+xlabel('Studentas');
+ylabel('p');
+title('a)')
+
+xticks(1:6);
+xticklabels({'1','2','3','4','5','6'});
+
+legend('1 egz','2 egz','3 egz','4 egz','Location','northeast');
+
+ylim([0 10]);
+yticks([0 5 10]);
+grid on;
+
+% Diskretus duomenų vaizdavimas
+
+subplot(2,1,2);
+b = bar(A, 'stacked');
+
+b(1).FaceColor = [0 0 0];
+b(2).FaceColor = [0.3 0.3 0.3];
+b(3).FaceColor = [0.7 0.7 0.7];
+b(4).FaceColor = [1 1 1];
+
+xlabel('Studentas');
+ylabel('p');
+title('b)')
+
+xticks(1:6);
+xticklabels({'1','2','3','4','5','6'});
+
+yticks([0 10 20 30 40]);
+ylim([0 40]);
+
+legend('1 egz','2 egz','3 egz','4 egz','Location','northeast');
+
+grid on;
+
+%}
